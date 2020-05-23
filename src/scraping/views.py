f1e8881f4bb642719a0dc4065dd7c267d0453ea1
patr0ne <yalart420@gmail.com>
2020-05-23 20:36:26 +0600

    third commit: templates, forms, views

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import FindForm
+from .models import Vacancy
+
+
+def home_view(request):
+    # print(request.GET)
+    form = FindForm()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+    if city or language:
+        _filter = {}
+        if city:
+            _filter['city__slug'] = city
+        if language:
+            _filter['language__slug'] = language
+
+        qs = Vacancy.objects.filter(**_filter)
+    return render(request, 'scraping/home.html', {'object_list': qs, 'form': form})
